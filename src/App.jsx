37ee719ba560f1639b2ac050b3e7dc50def7182e
Camilla Gomes <camillatrgomes@hotmail.com>
2025-11-17 15:46:19 -0300

    criando os componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import NavBar from "./components/NavBar"
+import Hero from "./components/Hero"
+import Intro from "./components/intro"
 
 function App() {
 
   return (
       <div>
         <NavBar />
+        <Hero />
+        <Intro />
+
       </div>
   )
 }
