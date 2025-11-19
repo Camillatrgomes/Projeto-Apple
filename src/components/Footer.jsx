ea08aa33f630fe0772f68e2b72f9017ed36c8872
Camilla Gomes <camillatrgomes@hotmail.com>
2025-11-19 09:30:52 -0300

    ajustes finais

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -22,38 +22,44 @@ function Footer() {
     const buttonLinks = ['Políticas de Privacidade', 'Termos de uso', 'Vendas' ]
     
     return (
-        <footer className="bg-gray-900 border-t border-gray-800">
-            <div className="max-w-7xl mx-auto px-6 py-12">
-                <div className="grid md:grid-cols-4 gap-8 mb-8">
-                    {sections.map((section, index) => (
-                        <div key={index}>
-                            <h4 className="font-semibold mb-4">{section.title}</h4>
+      <footer className="bg-gray-900 border-t border-gray-800">
+        <div className="max-w-7xl mx-auto px-6 py-12">
+          <div className="grid md:grid-cols-4 gap-8 mb-8">
+            {sections.map((section, index) => (
+              <div key={index}>
+                <h4 className="font-semibold mb-4">{section.title}</h4>
 
-                            <ul className=" space-y-2 text-sm text-gray-400">
-                                {section.links.map((link,linkIndex) =>(
-                                <li key={linkIndex}>
-                                    <a href="#" className="hover:text-white cursor-pointer">{link}</a>
-                                </li>
-                                ))}
-
-                            </ul>
-
-                        </div>
-                    ))}
-                </div>
-                <div className="border-t border-gray-800 pt-6">
-                <div className="flex flex-col md:flex-row justify-between items-center gap-4">
-                    <p className="text-xs text-gray-500">Copyright @2025 Apple Inc. Todos os direitos reservados</p>
-                   <div className="flex gap-6 text-sm text-gray-400">
-                     {buttonLinks.map((button, index) => (
-                        <a href="#" key={index} className="hover:text-white">{button}</a>
-                    ))}
-                   </div>
-                </div>
-                <p className="text-xs text-gray-500 mt-6 text-center">Site criado para fins educacionais</p>
+                <ul className=" space-y-2 text-sm text-gray-400">
+                  {section.links.map((link, linkIndex) => (
+                    <li key={linkIndex}>
+                      <a href="#" className="hover:text-white cursor-pointer">
+                        {link}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
+          <div className="border-t border-gray-800 pt-6">
+            <div className="flex flex-col md:flex-row justify-between items-center gap-4">
+              <p className="text-xl text-gray-300">
+                Copyright @2025 Apple Inc. Todos os direitos reservados
+              </p>
+              <div className="flex gap-6  text-gray-400">
+                {buttonLinks.map((button, index) => (
+                  <a href="#" key={index} className="hover:text-white">
+                    {button}
+                  </a>
+                ))}
+              </div>
             </div>
-            </div>
-        </footer>
-    )
+            <p className="text-xs text-gray-500 mt-6 text-center">
+              Site criado para fins educacionais
+            </p>
+          </div>
+        </div>
+      </footer>
+    );
 }
 export default Footer
